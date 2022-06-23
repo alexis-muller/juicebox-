9335e3b5c518e11b915cbb14162241c9d540a236
Alexis Muller <luis.martinez@sjsu.edu>
2022-06-22 17:02:18 -0700

    more changes

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // inside index.js
 //To run code npm run start:dev
+
+require("dotenv").config();
+
+// remove this once you confirm it works
+// console.log("JWT", process.env.JWT_SECRET);
+// like, seriously. go delete that!
+
+// EVERYTHING ELSE
+
 const PORT = 3000;
 const express = require("express");
 const server = express();
@@ -14,6 +23,14 @@ server.use("/api", apiRouter);
 
 const { client } = require("./db");
 client.connect();
+
+server.use((req, res, next) => {
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
+
+  next();
+});
 
 server.listen(PORT, () => {
   console.log("The server is up on port", PORT);
